6323695ddbe8980a39f2d90ae838e0fbc6fb1b51
DevLudo <roux.ludovic.pro@gmail.com>
2025-06-27 22:33:55 +0200

    Update nav-user.tsx

--- components/application/navigation/nav-user.tsx
+++ components/application/navigation/nav-user.tsx
@@ -32,7 +32,7 @@ export default function NavUser({
                         >
                             <Avatar className="size-8 rounded-full">
                                 <AvatarImage src={user.avatar} alt={user.name} />
-                                <AvatarFallback className="rounded-full">{user.name.charAt(0)}</AvatarFallback>
+                                <AvatarFallback className="rounded-full bg-primary text-primary-foreground">{user.name.charAt(0)}</AvatarFallback>
                             </Avatar>
                             <div className="grid flex-1 text-left text-sm leading-tight">
                                 <span className="truncate font-medium">{user.name}</span>
@@ -51,7 +51,7 @@ export default function NavUser({
                             <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                                 <Avatar className="size-8 rounded-lg">
                                     <AvatarImage src={user.avatar} alt={user.name} />
-                                    <AvatarFallback className="rounded-lg">{user.name.charAt(0)}</AvatarFallback>
+                                    <AvatarFallback className="rounded-full bg-primary text-primary-foreground">{user.name.charAt(0)}</AvatarFallback>
                                 </Avatar>
                                 <div className="grid flex-1 text-left text-sm leading-tight">
                                     <span className="truncate font-medium">{user.name}</span>
